ae25c695524d776265c6d546e87607ef682e3df7
Codetancy <codetancy@gmail.com>
2021-09-09 02:09:33 -0400

    Adding TestRepository interface

---
+++ src/Web/Repositories/Interfaces/ITestRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Repositories.Interfaces
+{
+    public interface ITestRepository
+    {
+        Task<List<Test>> GetTestsAsync();
+        Task<Test> GetTestByIdAsync(int testId);
+        Task<bool> CreateTestAsync(Test test);
+        Task<bool> UpdateTest(Test test);
+        Task<bool> DeleteTest(int testId);
+    }
+}
