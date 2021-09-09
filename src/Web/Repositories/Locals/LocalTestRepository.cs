e8ad5ad040e33673dabe4c69ea891ec5b55e1e48
Codetancy <codetancy@gmail.com>
2021-09-09 02:10:21 -0400

    Creating in memory TestRepository

---
+++ src/Web/Repositories/Locals/LocalTestRepository.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+using Web.Repositories.Interfaces;
+
+namespace Web.Repositories.Locals
+{
+    public class LocalTestRepository : ITestRepository
+    {
+        private readonly List<Test> _tests;
+        
+        public LocalTestRepository()
+        {
+            _tests = new List<Test>
+            {
+                new() { Id = 1, Text = "First" },
+                new() { Id = 2, Text = "Second" },
+                new() { Id = 3, Text = "Third" }
+            };
+        }
+        
+        public async Task<List<Test>> GetTestsAsync()
+        {
+            return await Task.FromResult(_tests);
+        }
+
+        public async Task<Test> GetTestByIdAsync(int testId)
+        {
+            var test = _tests.SingleOrDefault(p => p.Id == testId);
+            return await Task.FromResult(test);
+        }
+
+        public async Task<bool> CreateTestAsync(Test test)
+        {
+            var existingTest = await GetTestByIdAsync(test.Id);
+            if (existingTest is not null)
+                return await Task.FromResult(false);
+            
+            _tests.Add(test);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateTest(Test test)
+        {
+            var index = _tests.FindIndex(p => p.Id == test.Id);
+
+            if (index < 0)
+                return await Task.FromResult(false);
+
+            _tests[index] = test;
+            return true;
+        }
+
+        public async Task<bool> DeleteTest(int testId)
+        {
+            var index = _tests.FindIndex(p => p.Id == testId);
+
+            if (index < 0)
+                return await Task.FromResult(false);
+            
+            _tests.RemoveAt(index);
+            return true;
+        }
+    }
+}
