301517b66e568408d9392a4446019810cc4a04fe
Codetancy <codetancy@gmail.com>
2021-10-14 15:56:56 -0400

    Extending author, cart, and wishlist controller

--- src/Web/Controllers/V1/AuthorController.cs
+++ src/Web/Controllers/V1/AuthorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Web.Contracts.V1.Requests;
 using Web.Repositories.Interfaces;
 
 namespace Web.Controllers.V1
@@ -16,6 +17,7 @@ namespace Web.Controllers.V1
             _authorRepository = authorRepository;
         }
 
+        // GET api/v1/authors
         [HttpGet]
         public async Task<IActionResult> GetAllAuthors()
         {
@@ -23,15 +25,34 @@ namespace Web.Controllers.V1
             return Ok(authors);
         }
 
+        // GET api/v1/authors/{authorId}
         [HttpGet("{authorId:guid}")]
-        public async Task<IActionResult> GetBookById([FromRoute] Guid authorId)
+        public async Task<IActionResult> GetAuthorById([FromRoute] Guid authorId)
         {
             var author = await _authorRepository.GetAuthorByIdAsync(authorId);
-
-            if (author is null)
-                return BadRequest();
-
-            return Ok(author);
+            return author is null ? NotFound() : Ok(author);
         }
+
+        // POST api/v1/authors
+        [HttpPost]
+        public async Task<IActionResult> CreateAuthor([FromBody] CreateAuthorRequest request)
+        {
+            return Ok();
+        }
+
+        // PUT api/v1/authors/{authorId}
+        [HttpPut("{authorId:guid}")]
+        public async Task<IActionResult> UpdateAuthor([FromRoute] Guid authorId, [FromBody] UpdateAuthorRequest request)
+        {
+            return Ok();
+        }
+
+        // DELETE api/v1/authors/{authorId}
+        [HttpDelete("{authorId:guid}")]
+        public async Task<IActionResult> RemoveAuthor([FromRoute] Guid authorId)
+        {
+            return Ok();
+        }
+
     }
 }
--- src/Web/Controllers/V1/CartController.cs
+++ src/Web/Controllers/V1/CartController.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Web.Contracts.V1.Requests;
+using Web.Extensions;
+using Web.Models;
 using Web.Repositories.Interfaces;
 
 namespace Web.Controllers.V1
 {
+    [Authorize]
     [ApiController]
     [Route("api/v1/cart")]
     public class CartController : ControllerBase
@@ -20,21 +25,40 @@ namespace Web.Controllers.V1
         [HttpGet]
         public async Task<IActionResult> GetUserCart()
         {
-            var userId = Guid.Parse("3304f724-dbfd-45f3-9737-ccd0b28929d6");
+            var userId = HttpContext.GetUserId();
             var cart = await _cartRepository.GetCartByUserIdAsync(userId);
-            return Ok(cart);
+            return cart is null ? NotFound("There is no cart for the current user") : Ok(cart);
         }
 
-        // POST api/v1/cart/books/{bookId}
-        [HttpPost("books/{bookId:guid}")]
-        public async Task<IActionResult> AddBookToCart([FromRoute] Guid bookId)
+        // POST api/v1/cart
+        [HttpPost]
+        public async Task<IActionResult> CreateCart()
         {
-            var cartId = Guid.Parse("3304f724-dbfd-45f3-9737-ccd0b28929d6");
+            var userId = HttpContext.GetUserId();
+            var cart = new Cart { UserId = userId };
+            bool created = await _cartRepository.CreateCartForUserAsync(userId, cart);
+            return created
+                ? CreatedAtAction(nameof(GetUserCart), new {cartId = cart.CartId}, cart)
+                : BadRequest();
+        }
+
+        // POST api/v1/cart/books
+        [HttpPost("books")]
+        public async Task<IActionResult> AddBookToCart([FromBody] AddBookToCartRequest request)
+        {
+            (Guid bookId, Guid cartId) = request;
             var books = await _cartRepository.AddBookToCart(cartId, bookId);
 
             return books == null
                 ? NotFound($"Book with ID {bookId} does not exist.")
                 : Ok(books);
+        }
+
+        [HttpDelete("books/{bookId:guid}")]
+        public async Task<IActionResult> RemoveBookFromCart(
+            [FromRoute] Guid bookId, [FromBody] RemoveBookFromCartRequest request)
+        {
+            return Ok();
         }
     }
 }
