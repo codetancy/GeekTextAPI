c298b5756d753316f9288c489ced47a458335bfd
Codetancy <codetancy@gmail.com>
2021-11-07 23:11:13 -0500

    Implement PagedResponse as a class instead of a record

--- src/Web/Contracts/V1/Responses/PagedResponse.cs
+++ src/Web/Contracts/V1/Responses/PagedResponse.cs
@@ -2,6 +2,23 @@ using System.Collections.Generic;
 
 namespace Web.Contracts.V1.Responses
 {
-    public record PagedResponse<T>(IEnumerable<T> Data, int PageNumber, int PageSize, string PreviousPage,
-        string NextPage) where T : class;
+    public class PagedResponse<T> where T : class
+    {
+        public IEnumerable<T> Data { get; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+        public string PreviousPage { get; }
+        public string NextPage { get; }
+
+        public PagedResponse(IEnumerable<T> data, int pageNumber, int pageSize, string previousPage, string nextPage)
+        {
+            Data = data;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            PreviousPage = previousPage;
+            NextPage = nextPage;
+        }
+
+    }
+
 }
