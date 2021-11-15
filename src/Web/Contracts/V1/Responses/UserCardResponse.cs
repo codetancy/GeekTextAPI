b137ca8b972921507ecc7a79e66d05f73846b0e4
Codetancy <codetancy@gmail.com>
2021-11-15 00:33:31 -0500

    Add constructor to user card response

--- src/Web/Contracts/V1/Responses/UserCardResponse.cs
+++ src/Web/Contracts/V1/Responses/UserCardResponse.cs
@@ -1,11 +1,20 @@
-using System;
 using System.Collections.Generic;
 
 namespace Web.Contracts.V1.Responses
 {
     public class UserCardResponse : Response
     {
-        public Guid UserId { get; init; }
-        public IEnumerable<SimpleCardResponse> Cards { get; set; }
+        public UserCardResponse()
+        {
+        }
+
+        public UserCardResponse(string userName, IEnumerable<SimpleCardResponse> cards)
+        {
+            UserName = userName;
+            Cards = cards;
+        }
+
+        public string UserName { get; init; }
+        public IEnumerable<SimpleCardResponse> Cards { get; init; }
     }
 }
