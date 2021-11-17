7d1c9ace48d8d108bc2cdb880cc0dbf2e9ba9f12
Codetancy <codetancy@gmail.com>
2021-11-16 21:10:47 -0500

    Add documentation

--- src/Web/Controllers/V1/UserController.cs
+++ src/Web/Controllers/V1/UserController.cs
@@ -16,7 +16,7 @@ using Web.Services.Interfaces;
 namespace Web.Controllers.V1
 {
     [ApiController]
-    [Authorize]
+    [Authorize(Roles = "Regular")]
     [Route("api/v1/users")]
     public class UserController : ControllerBase
     {
@@ -38,7 +38,7 @@ namespace Web.Controllers.V1
         /// Get user by user name
         /// </summary>
         /// <param name="userName">User to retrieve</param>
-        /// <response code="200">Successful operation</response>
+        /// <response code="200">Requested user</response>
         /// <response code="404">User not found</response>
         [HttpGet("{userName}")]
         [AllowAnonymous]
@@ -50,6 +50,20 @@ namespace Web.Controllers.V1
                 err => err.GetResultFromError());
         }
 
+        /// <summary>Update user information</summary>
+        /// <remarks>
+        /// 1. User must be logged in.
+        /// 2. User can only update its own profile.
+        /// </remarks>
+        /// <param name="userName">User to update</param>
+        /// <param name="request">
+        /// User with updated fields.
+        /// For full list of fields refer to the UpdateUserRequest schema.
+        /// </param>
+        /// <inheritdoc cref="UpdateUserRequest"/>
+        /// <response code="200">User updated successfully</response>
+        /// <response code="400">Invalid fields</response>
+        /// <response code="401">User does not own resource</response>
         [HttpPut("{userName}")]
         public async Task<IActionResult> UpdateUser(string userName, UpdateUserRequest request)
         {
@@ -64,6 +78,18 @@ namespace Web.Controllers.V1
                 err => err.GetResultFromError());
         }
 
+        /// <summary>
+        /// Get list of user cards
+        /// </summary>
+        /// <remarks>
+        /// 1. User must be logged in.
+        /// 2. User can only see its own cards.
+        /// </remarks>
+        /// <param name="userName">User to search</param>
+        /// <response code="200">List of user cards</response>
+        /// <response code="400">Invalid username</response>
+        /// <response code="401">User does not own resource</response>
+        /// <response code="404">User not found</response>
         [HttpGet("{userName}/cards")]
         public async Task<IActionResult> GetUserCards(string userName)
         {
@@ -84,6 +110,23 @@ namespace Web.Controllers.V1
                 error => error.GetResultFromError());
         }
 
+        /// <summary>
+        /// Create a card for a user
+        /// </summary>
+        /// <remarks>
+        /// 1) User must be logged in.
+        /// 2) User can only add cards to itself.
+        /// </remarks>
+        /// <param name="userName">User to add a card to</param>
+        /// <param name="request">
+        /// Card to create.
+        /// For full list of fields see CreateCardRequest schema.
+        /// </param>
+        /// <inheritdoc cref="CreateCardRequest"/>
+        /// <response code="200">Card created successfully</response>
+        /// <response code="400">Unable to create card</response>
+        /// <response code="401">User does not own resource</response>
+        /// <response code="404">User/card not found</response>
         [HttpPost("{userName}/cards")]
         public async Task<IActionResult> CreateCard(string userName, [FromBody] CreateCardRequest request)
         {
@@ -98,6 +141,19 @@ namespace Web.Controllers.V1
             return result.Match(Ok, error => error.GetResultFromError());
         }
 
+        /// <summary>
+        /// Delete a card from a user
+        /// </summary>
+        /// <remarks>
+        /// 1) User must be logged in
+        /// 2) User can only delete its own cards
+        /// </remarks>
+        /// <param name="userName">User to search</param>
+        /// <param name="cardId">Id of the card to delete</param>
+        /// <response code="204">Card deleted successfully</response>
+        /// <response code="400">Unable to delete card</response>
+        /// <response code="401">User does not own card</response>
+        /// <response code="404">User/card not found</response>
         [HttpDelete("{userName}/cards/{cardId:guid}")]
         public async Task<IActionResult> DeleteUserCard(string userName, Guid cardId)
         {
